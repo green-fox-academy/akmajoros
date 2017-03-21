778ea2e6fa1ab96ab4f70ea94767c6da067b473b
Kristof Majoros <akmajoros1@gmail.com>
2017-03-21 12:19:40 +0100

    created DefineBasicInfo and FavoriteNumber

---
+++ week-02/day-02/src/DefineBasicInfo.java
@@ -0,0 +1,14 @@
+ public class DefineBasicInfo {
+
+        public static void main(String[] args) {
+            String myName = "Kristof Majoros";
+            int myAge = 25;
+            double myHeight = 1.85;
+            boolean married = false;
+            System.out.println(myName);
+            System.out.println(myAge);
+            System.out.println(myHeight);
+            System.out.println(married);
+        }
+
+}
---
+++ week-02/day-02/src/FavoriteNumber.java
@@ -0,0 +1,7 @@
+public class FavoriteNumber {
+    public static void main(String[] args) {
+
+        int a = 8;
+        System.out.println("My favourite number is : " + a);
+    }
+}
