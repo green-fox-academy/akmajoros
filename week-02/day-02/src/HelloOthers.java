21e19255d44e7aae8297583b9d1943f72da91283
Kristof Majoros <akmajoros1@gmail.com>
2017-03-21 11:30:58 +0100

    created Helloworld, HelloMe, HelloOthers and .gitignore

---
+++ week-02/day-02/src/HelloOthers.java
@@ -0,0 +1,10 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        {
+            System.out.println("Hello Nora!");
+            System.out.println("Hello Noemi!");
+            System.out.println("Hello Dani!");
+        }
+
+    }
+}
