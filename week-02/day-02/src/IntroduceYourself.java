5949250f20db6d8e7243494a429aafd08e2f24f1
Kristof Majoros <akmajoros1@gmail.com>
2017-03-21 11:41:42 +0100

    created HumptyDumpty and IntroduceYourself

---
+++ week-02/day-02/src/IntroduceYourself.java
@@ -0,0 +1,10 @@
+public class IntroduceYourself {
+
+    public static void main(String[] args) {
+
+        System.out.println("Kristof Majoros");
+        System.out.println(25);
+        System.out.println(1.85);
+    }
+
+}
