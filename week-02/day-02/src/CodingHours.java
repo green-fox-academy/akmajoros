e52899111916fe88cd9986037c4803cad4d723df
Kristof Majoros <akmajoros1@gmail.com>
2017-03-21 12:05:17 +0100

    created CodingHours

---
+++ week-02/day-02/src/CodingHours.java
@@ -0,0 +1,7 @@
+public class CodingHours {
+    public static void main(String[] args) {
+
+        System.out.println(17 * 5 * 6);
+        System.out.println((17 * 5 * 6.) / (17 * 52));
+    }
+}
--- week-02/day-02/src/HelloOthers.java
+++ week-02/day-02/src/HelloOthers.java
@@ -1,10 +1,9 @@
 public class HelloOthers {
     public static void main(String[] args) {
-        {
+
             System.out.println("Hello Nora!");
             System.out.println("Hello Noemi!");
             System.out.println("Hello Dani!");
-        }
 
     }
 }
