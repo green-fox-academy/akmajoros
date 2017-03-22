8a9fa8bd03dd828acfb26abb50f5a957c2676fb9
Kristof Majoros <akmajoros1@gmail.com>
2017-03-22 14:10:38 +0100

    created AppendA and Sum

---
+++ week-02/day-03/src/AppendA.java
@@ -0,0 +1,9 @@
+public class AppendA {
+  public static void main(String[] args) {
+    String am = "kuty";
+    System.out.println(letterA(am));
+  }
+  public static String letterA (String text){
+    return (text + "a");
+  }
+}
---
+++ week-02/day-03/src/Sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Sum {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+    System.out.println("Please give me a number!");
+    int chosenNumber = scanner.nextInt();
+    System.out.println(sumNumbers(chosenNumber));
+  }
+  public static int sumNumbers (int number) {
+    int a2 = 0;
+    for(int a = 0; a < number; a++){//a+=(a+1){
+      a2 = a + a2;
+      System.out.println(a);
+    }
+    return a2;
+  }
+}
