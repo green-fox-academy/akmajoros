fe3b1100196bc9ef02301c61d28719a5c87c8768
Kristof Majoros <akmajoros1@gmail.com>
2017-03-23 16:53:21 +0100

    created CandyShop and ElementFinder

--- week-02/day-04/src/AppendLetter.java
+++ week-02/day-04/src/AppendLetter.java
@@ -6,11 +6,31 @@ public class AppendLetter {
     ArrayList<String> far = new ArrayList<String>(Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
     int farSize = far.size();
 
+    //Version1
     for (int i = 0; i < farSize; i++){
       far.set(i, far.get(i)+"a");
     }
 
-      System.out.println(far);
+    //Version2
+    /*int i = 0;
+    for(String currentElement : far) {
+      String newName = currentElement+"a";
+      far.set(i, newName);
+      i++;
+    }*/
+
+    //Version3
+    /*ArrayList<String> newList = new ArrayList();
+    for(String currentElement : far){
+
+      String newName = currentElement +  "a";
+      newList.add(newName);
+    }
+    far = newList;*/
+
+    System.out.println(far);
+
+    //System.out.println(newList);
   }
 }
 
