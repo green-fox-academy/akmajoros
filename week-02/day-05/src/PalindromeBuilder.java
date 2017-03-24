882ce239725cf7ae72a596c07b8aacbc138ad8a6
Kristof Majoros <akmajoros1@gmail.com>
2017-03-24 14:38:34 +0100

    created a function that bulids Palindromes using userinput

--- week-02/day-05/src/PalindromeBuilder.java
+++ week-02/day-05/src/PalindromeBuilder.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class PalindromeBuilder {
   public static void main(String[] args) {
-    System.out.println("Please enter a word:");
+    System.out.println("Please give me a string: ");
     Scanner scanner = new Scanner(System.in);
-    String string = scanner.nextLine();
-
-    int length = string.length();
+    String string1 = scanner.nextLine();
+    System.out.println(createPalindrome(string1));
+  }
+  public static String createPalindrome (String incomingString){
+    int length = incomingString.length();
     for (int i = length-1; i >= 0 ; i--) {
-      string = string + (string.charAt(i));
+      incomingString = incomingString + (incomingString.charAt(i));
     }
-
+    return incomingString;
   }
 }
