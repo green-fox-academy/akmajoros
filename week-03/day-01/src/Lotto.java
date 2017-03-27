2713642e9220922bae25784a4d78efd036902263
Kristof Majoros <akmajoros1@gmail.com>
2017-03-27 14:47:31 +0200

    managed to calculate the occurence of each lotto number and put it into a hashnap

--- week-03/day-01/src/Lotto.java
+++ week-03/day-01/src/Lotto.java
@@ -1,6 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,16 +13,19 @@ public class Lotto {
     try {
       List<String> lines = Files.readAllLines(filePath);
       HashMap<String, Integer> winnerNumbers = new HashMap<String, Integer>();
-      int occurence = 0;
       for(String line : lines){
         String[] linesArray = line.split(";");
         int sizeTo = linesArray.length;
         int sizeFrom = sizeTo - 5;
         for (int i = sizeFrom; i < sizeTo; i++){
-          winnerNumbers.put(linesArray[i], occurence);
+          if (winnerNumbers.containsKey(linesArray[i])){
+            winnerNumbers.put(linesArray[i], winnerNumbers.get(linesArray[i]) + 1);
+          } else {
+            winnerNumbers.put(linesArray[i], 0);
+          }
         }
-        System.out.println(winnerNumbers);
       }
+      System.out.println(winnerNumbers);
     } catch (Exception e) {
       System.out.println("Error occured!");
     }
