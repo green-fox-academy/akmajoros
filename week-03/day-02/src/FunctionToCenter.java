fa924a3913d1a1ad265ccc82f15c12bb8d88fe67
Kristof Majoros <akmajoros1@gmail.com>
2017-03-28 16:17:03 +0200

    created FunctionToCenter

--- week-03/day-02/src/FunctionToCenter.java
+++ week-03/day-02/src/FunctionToCenter.java
@@ -4,24 +4,28 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class Checkboard {
+public class FunctionToCenter {
 
   public static void mainDraw(Graphics graphics){
-    int size = 38;
-    int x = 38;
-    int y = 0;
-    for (int i = x; i < 300; i+=x*2) {
-      for (int j = 0; j < 300; j += x*2) {
-          graphics.setColor(Color.BLACK);
-          graphics.fillRect(i, j, size, size);
-      }
+    drawingLines(0, 0, graphics);
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a line from that point to the center of the canvas.
+    // fill the canvas with lines from the edges, every 20 px, to the center.
+
+
+
+  }
+  public static void drawingLines (int x, int y, Graphics graphics){
+    for (int i = 0; i < 300; i+=20) {
+      graphics.setColor(Color.BLACK);
+      graphics.drawLine(i, 0, 150, 150);
+      graphics.drawLine(0, i, 150, 150);
+      graphics.drawLine(i, 320, 150, 150);
+      graphics.drawLine(320, i, 150, 150);
     }
-    for (int i = 0; i < 300; i+=x*2) {
-      for (int j = x; j < 300; j += x*2) {
-        graphics.setColor(Color.BLACK);
-        graphics.fillRect(i, j, size, size);
-      }
-    }
+
+
   }
 
   //    Don't touch the code below
