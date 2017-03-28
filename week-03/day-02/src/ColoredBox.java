29e3cb81b3c637b3535d4695d122d5bd6dead998
Kristof Majoros <akmajoros1@gmail.com>
2017-03-28 11:00:29 +0200

    created LinesInTheMiddle, GoToCenter, ColoredBox and Diagonals

---
+++ week-03/day-02/src/ColoredBox.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ColoredBox {
+  public static void mainDraw(Graphics graphics){
+    graphics.setColor(Color.BLUE);
+    graphics.drawLine(25, 25, 100, 25 );
+    graphics.setColor(Color.GREEN);
+    graphics.drawLine(100, 25, 100, 100);
+    graphics.setColor(Color.RED);
+    graphics.drawLine(100, 100, 25, 100);
+    graphics.setColor(Color.YELLOW);
+    graphics.drawLine(25, 100, 25, 25);
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
