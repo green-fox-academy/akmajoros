bd60886007ac624d93dd1c4df7aba9a55d5130da
Kristof Majoros <akmajoros1@gmail.com>
2017-03-30 14:42:12 +0200

    created String1 and StringAgain

--- week-03/day-04/src/StringAgain.java
+++ week-03/day-04/src/StringAgain.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class String1 {
+public class StringAgain {
   public static void main(String[] args) {
     String words = "xxxoxoxo?";
     char from = 'x';
