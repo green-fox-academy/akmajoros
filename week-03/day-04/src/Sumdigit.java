5e5d5cfb1ae0ebc68a690d62e471a9af35754971
Kristof Majoros <akmajoros1@gmail.com>
2017-03-30 10:38:58 +0200

    created Sumdigit

--- week-03/day-04/src/Sumdigit.java
+++ week-03/day-04/src/Sumdigit.java
@@ -1,8 +1,13 @@
 public class Sumdigit {
   public static void main(String[] args) {
-    System.out.println(sumdigit());
+    System.out.println(sumdigit(1264));
   }
   public static int sumdigit(int n) {
-    if (n == 1)
+    if (n < 10){
+      return n;
+    }
+    else {
+      return (n % 10) + (sumdigit(n/10));
+    }
   }
 }
