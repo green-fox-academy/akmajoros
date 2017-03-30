8e25e2e8d5c05aea7a9a596a660af99d3a67fd48
Kristof Majoros <akmajoros1@gmail.com>
2017-03-30 15:36:53 +0200

    created Fibonacci

--- week-03/day-04/src/Fibonacci.java
+++ week-03/day-04/src/Fibonacci.java
@@ -1,5 +1,12 @@
 public class Fibonacci {
   public static void main(String[] args) {
-    
+    System.out.println(fibonacci(7));
+  }
+  public static int fibonacci(int  n) {
+    if ((n == 1) || (n == 0)) {
+      return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
   }
 }
+
