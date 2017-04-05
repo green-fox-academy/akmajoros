2c4895a73c49789215bd1d738f4fa703b3aaf273
Kristof Majoros <akmajoros1@gmail.com>
2017-04-05 12:18:44 +0200

    added null test to SumTest

--- week-04/day-03/src/SumTesting/SumTest.java
+++ week-04/day-03/src/SumTesting/SumTest.java
@@ -30,4 +30,22 @@ class SumTest {
     toAdd.add(38);
     assertEquals(38, sum2.sumMethod(toAdd));
   }
+
+  @Test
+  public void multipleElement(){
+    ArrayList<Integer> toAdd = new ArrayList<>();
+    Sum sum2 = new Sum();
+    toAdd.add(38);
+    toAdd.add(10);
+    toAdd.add(30);
+    assertEquals(78, sum2.sumMethod(toAdd));
+  }
+
+  @Test
+  public void nullTest(){
+    ArrayList<Integer> toAdd = null;
+    assertEquals(null, toAdd);
+  }
+
+
 }
