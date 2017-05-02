3d7ddebda1819c3bfe50efa818f207fe61bb2f0e
Kristof Majoros <akmajoros1@gmail.com>
2017-05-02 10:28:35 +0200

    I don't know what this thing is, probably some stuff from week06

--- week-06/day-02/src/Car.java
+++ week-06/day-02/src/Car.java
@@ -5,27 +5,18 @@ public class Car {
   public enum carType {VOLVO, MERCEDES, AUDI, ALFAROMEO};
   public enum carColor {BLACK, SILVER, RED, WHITE};
 
-  public Car (carType type, carColor color) {
+  carType type;
+  carColor color;
+
+  public Car {
+    int randomType = (int)(Math.random() * carType.values().length);
+    int randomColor = (int)(Math.random() * carColor.values().length);
+    type = carType.values()[randomType;
+    color = carColor.values()[randomColor];
   }
 
   List<Car> listOfCars = new ArrayList<>();
 
-  public void carFiller() {
-    for (int i = 0; i < 256; i++) {
-      int randomType = (int) Math.random()*4;
-      int randomColor = (int) Math.random()*4;
-      carType type;
-      carColor color;
-
-      for (int j = 0; j < carType.values().length; j++) {
-        type = carType.values()[randomType];
-      }
-
-      for (int k = 0; k < carColor.values().length; k++) {
-        color = carColor.values()[randomColor];
-      }
-
-      Car car = new Car(type, color);
-    }
+  public static void main(String[] args) {
   }
 }
