1f8c56fde881308636fe866d44bf40fb8f8506de
Kristof Majoros <akmajoros1@gmail.com>
2017-05-09 11:31:00 +0200

    todoWithH2 initial commit

---
+++ week-08/day-02/todoWithH2/src/main/java/com/greenfoxacademy/todoWithH2/TodoWithH2Application.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.todoWithH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWithH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWithH2Application.class, args);
+	}
+}
---
+++ week-08/day-02/todoWithH2/src/test/java/com/greenfoxacademy/todoWithH2/TodoWithH2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.todoWithH2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoWithH2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
